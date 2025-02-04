4217813af4a5b288fee3fe0071400fc733510871
ckt22 <tangck0202@gmail.com>
2025-02-04 22:09:18 +0800

    fix: tailwind v4 changes; welcome screen

--- src/components/WelcomeScreen/index.tsx
+++ src/components/WelcomeScreen/index.tsx
@@ -1,10 +1,35 @@
 export default function WelcomeScreen() {
   return (
     <div className="flex items-center justify-center h-full flex-col">
-      <h1 className="uppercase shadow-custom border-custom border-black text-white text-5xl">
+      <img src="/victory.png" className="w-56 h-40 object-contain" />
+      <h1
+        style={{
+          textShadow: " -5px 3px 0px #000000",
+          WebkitTextFillColor: "white",
+          WebkitTextStroke: "2px black",
+        }}
+        className="uppercase text-6xl md:text-8xl italic tracking-tighter"
+      >
         defi saga
       </h1>
-      <h3 className="uppercase text-white">your personal defi assistant</h3>
+      <h3
+        style={{
+          textShadow: " -1px 4px 0px #000000",
+          WebkitTextFillColor: "white",
+          WebkitTextStroke: "1px black",
+        }}
+        className="uppercase text-white text-xl md:text-3xl tracking-tighter"
+      >
+        your personal defi assistant
+      </h3>
+      {/* BUTTON */}
+      <button
+        type="button"
+        style={{ boxShadow: "-1px 4px 0px 0px #000000" }}
+        className="parallelogram border-2 border-black border-solid bg-background-yellow py-4 px-20 uppercase text-white text-2xl tracking-tighter rounded-sm mt-5 cursor-pointer"
+      >
+        <span>Start</span>
+      </button>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,24 +6,7 @@ const config: Config = {
     "./src/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        lilita: ['"Lilita One"', "cursive"],
-      },
-      colors: {
-        "background-gray": "#353C52",
-        "background-light-blue": "#248EF6",
-        "background-blue": "#105DE5",
-      },
-      boxShadow: {
-        custom: "-3.16px 12.63px 0px 0px #000000",
-      },
-      borderWidth: {
-        custom: "5.05px",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
 
