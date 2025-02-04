744aa56029667afdc42dc645aa797d14be3eb0c5
ckt22 <tangck0202@gmail.com>
2025-02-04 22:59:27 +0800

    add: welcome modal styles

--- src/components/OnboardingModal/index.tsx
+++ src/components/OnboardingModal/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, Dispatch, SetStateAction } from "react";
 import {
   Dialog,
   DialogBackdrop,
@@ -12,72 +12,92 @@ import {
   XMarkIcon,
 } from "@heroicons/react/24/outline";
 
-export default function OnboardingModal() {
-  const [open, setOpen] = useState(true);
+export default function OnboardingModal({
+  openModal,
+  setOpenModal,
+}: {
+  openModal: boolean;
+  setOpenModal: Dispatch<SetStateAction<boolean>>;
+}) {
+  const [step, setStep] = useState(1);
 
   return (
-    <Dialog open={open} onClose={setOpen} className="relative z-10">
+    <Dialog
+      open={openModal}
+      onClose={setOpenModal}
+      className="relative z-10 overflow-visible"
+    >
       <DialogBackdrop
         transition
-        className="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"
+        className="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in overflow-visible"
       />
 
-      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+      <div className="fixed inset-0 z-10 w-screen overflow-visible">
+        <div className="flex sm:min-h-full justify-center p-4 text-center sm:items-center sm:p-0 overflow-visible">
           <DialogPanel
             transition
-            className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:p-6 data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+            className="sm:min-w-[45vw] relative transform overflow-visible bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95"
           >
-            <div className="absolute top-0 right-0 hidden pt-4 pr-4 sm:block">
-              <button
-                type="button"
-                onClick={() => setOpen(false)}
-                className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden"
+            <div className="bg-background-light-blue text-center relative py-2 overflow-visible">
+              <DialogTitle
+                as="h2"
+                style={{
+                  textShadow: " -1px 2px 0px #000000",
+                  WebkitTextFillColor: "white",
+                  WebkitTextStroke: "0.2px black",
+                }}
+                className="text-3xl uppercase font-semibold text-gray-900"
               >
-                <span className="sr-only">Close</span>
-                <XMarkIcon aria-hidden="true" className="size-6" />
-              </button>
+                Getting Started
+              </DialogTitle>
+
+              <div className="absolute top-[10px] right-[-10px] overflow-visible">
+                <button
+                  type="button"
+                  onClick={() => setOpenModal(false)}
+                  className="rounded-md z-50 parallelogram bg-red-700 p-1 text-white hover:bg-red-800 transition-colors"
+                >
+                  <span className="sr-only">Close</span>
+                  <XMarkIcon aria-hidden="true" className="size-5" />
+                </button>
+              </div>
             </div>
 
-            <div className="sm:flex sm:items-start">
-              <div className="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:size-10">
-                <ExclamationTriangleIcon
-                  aria-hidden="true"
-                  className="size-6 text-red-600"
-                />
-              </div>
-              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <DialogTitle
-                  as="h3"
-                  className="text-base font-semibold text-gray-900"
-                >
-                  Deactivate account
-                </DialogTitle>
-                <div className="mt-2">
-                  <p className="text-sm text-gray-500">
-                    Are you sure you want to deactivate your account? All of
-                    your data will be permanently removed from our servers
-                    forever. This action cannot be undone.
+            <div className="bg-background-blue min-h-[85vh] sm:min-h-[45vh] pb-10 pt-3 overflow-visible flex flex-col">
+              <div className="flex flex-col sm:flex-row flex-1">
+                <div className="flex items-center justify-center sm:mr-5">
+                  <img
+                    src="/victory.png"
+                    className="w-56 h-40 object-contain"
+                  />
+                </div>
+                <div className="px-3 sm:px-0 text-left items-center sm:items-start max-sm:justify-between text-white flex flex-col flex-1 max-sm:min-h-[60vh]">
+                  <p
+                    className="text-2xl"
+                    style={{
+                      textShadow: " -1px 2px 0px #000000",
+                      WebkitTextFillColor: "white",
+                      WebkitTextStroke: "0.5px black",
+                    }}
+                  >
+                    Deposit Funds to Activate Your AI Strategist
                   </p>
+                  <p className="mt-5">
+                    Before your AI assistant can start optimizing your funds,
+                    you need to deposit some USDC or USDT.
+                  </p>
+                  <p className="max-sm:self-start mt-5">🔹 Why?</p>
+                  <p>
+                    This deposit will be used for executing DeFi strategies and
+                    covering transaction fees when needed.
+                  </p>
+                  <button className="self-center max-sm:mt-auto">
+                    Connect Wallet
+                  </button>
                 </div>
               </div>
             </div>
-            <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-              <button
-                type="button"
-                onClick={() => setOpen(false)}
-                className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 sm:ml-3 sm:w-auto"
-              >
-                Deactivate
-              </button>
-              <button
-                type="button"
-                onClick={() => setOpen(false)}
-                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto"
-              >
-                Cancel
-              </button>
-            </div>
+            <div className="bg-background-light-blue h-[2vh] overflow-visible" />
           </DialogPanel>
         </div>
       </div>
--- src/components/WelcomeScreen/index.tsx
+++ src/components/WelcomeScreen/index.tsx
@@ -1,6 +1,12 @@
+"use client";
+import { useState } from "react";
+import OnboardingModal from "../OnboardingModal";
+
 export default function WelcomeScreen() {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
-    <div className="flex items-center justify-center h-full flex-col">
+    <div className="relative flex items-center justify-center h-full flex-col">
       <img src="/victory.png" className="w-56 h-40 object-contain" />
       <h1
         style={{
@@ -25,11 +31,15 @@ export default function WelcomeScreen() {
       {/* BUTTON */}
       <button
         type="button"
+        onClick={() => setOpenModal(true)}
         style={{ boxShadow: "-1px 4px 0px 0px #000000" }}
         className="parallelogram border-2 border-black border-solid bg-background-yellow py-4 px-20 uppercase text-white text-2xl tracking-tighter rounded-sm mt-5 cursor-pointer"
       >
         <span>Start</span>
       </button>
+      {openModal && (
+        <OnboardingModal openModal={openModal} setOpenModal={setOpenModal} />
+      )}
     </div>
   );
 }
