830c45400bc279fefc6dfc7e531425dc579de0bb
ckt22 <tangck0202@gmail.com>
2025-02-04 21:24:01 +0800

    add: setting basics; background

---
+++ src/components/WelcomeScreen/index.tsx
@@ -0,0 +1,10 @@
+export default function WelcomeScreen() {
+  return (
+    <div className="flex items-center justify-center h-full flex-col">
+      <h1 className="uppercase shadow-custom border-custom border-black text-white text-5xl">
+        defi saga
+      </h1>
+      <h3 className="uppercase text-white">your personal defi assistant</h3>
+    </div>
+  );
+}
