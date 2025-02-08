c5ea88f8037f6dc85b65b57b66113fe455a89e82
LI-YONG-QI <a2369028@gmail.com>
2025-02-08 22:16:12 +0800

    feat: add morpho logic into deposit button

--- src/components/StakeScreen/index.tsx
+++ src/components/StakeScreen/index.tsx
@@ -1,93 +1,152 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 import CurrencyInput from "../CurrencyInput";
 import { useForm, Controller } from "react-hook-form";
+import type { TypedData } from "viem";
+import { EXECUTOR, USDC, USDC_DECIMAL } from "../../helpers/constants";
+import { signTypedData } from "@wagmi/core";
+import { config } from "../../config";
+import { baseSepolia } from "wagmi/chains";
+import { usdcAbi } from "../../abis/usdc";
+import { execution } from "../../helpers/mock-backend";
+import { useAccount, useReadContract } from "wagmi";
+import { createMorphoCall } from "../../helpers/strategy";
+import { waitForTransactionReceipt } from "@wagmi/core";
+
+const types = {
+  Permit: [
+    { name: "owner", type: "address" },
+    { name: "spender", type: "address" },
+    { name: "value", type: "uint256" },
+    { name: "nonce", type: "uint256" },
+    { name: "deadline", type: "uint256" },
+  ],
+} as const satisfies TypedData;
 
 interface DepositFormData {
-    deposit: {
-      currency: string;
-      amount: string;
-    };
-  }
+  deposit: {
+    currency: string;
+    amount: string;
+  };
+}
 
 interface StakeScreenProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
-async function onSubmit(data: DepositFormData) {
-    console.log("Form submitted with values:", data);
-    console.log("aaaa")
-  };
+const EXPIRY = 60000;
 
-export default function StakeScreen({ isOpen, onClose }: StakeScreenProps ) {
-    const { control, handleSubmit } = useForm<DepositFormData>();
-    const [inputValue, setinputValue] = useState(0);
-    
-    if (!isOpen) return null;
+function serializeAmount(amount: string, decimal: number) {
+  const floatAmount = parseFloat(amount);
+  if (isNaN(floatAmount)) {
+    throw new Error("Amount must be a valid number.");
+  }
 
-    return (
-        <>
-        <div className="fixed inset-0  flex items-center justify-center z-50">
-            <div className='bg-gray-900/80 absolute t-0 left-0 w-full h-full z-0'></div>
-            <div className="bg-[#1E90FF] text-white rounded-lg shadow-lg p-6 w-[90vw] max-w-[600px] z-10 relative">
-                {/* Header */}
-                <div className="flex justify-center items-center border-b border-white pb-2">
-                    <div className='items-center' >
-                        <span className='text-3xl inline-block'>🛡️</span>
-                        <img 
-                            src="/morpho/eventInfo.svg" 
-                            className="h-6 inline-block"
-                        />
-                    </div>
-                <img 
-                    src="/morpho/cancel.svg" 
-                    className="h-10 absolute -right-2 top-2 cursor-pointer"
-                    onClick={onClose}
-                />
-                </div>
+  const integerAmount = Math.floor(floatAmount * decimal);
+  return BigInt(integerAmount);
+}
 
-                {/* Body */}
-                <div className="mt-4 flex flex-col">
-                
-                    <img 
-                        src="/Stake/information.svg" 
-                        className="h-18"
-                    />
-                    <img 
-                        src="/Stake/intro.svg" 
-                        className="h-10 mt-10"
-                    />
-                    <form
-                        onSubmit={handleSubmit(onSubmit)}
-                        className="w-full flex flex-col gap-y-3"
-                      >
-                        <Controller
-                          name="deposit"
-                          control={control}
-                          rules={{
-                            required: "Please enter an amount",
-                            validate: {
-                              positive: (value) =>
-                                parseFloat(value.amount) > 0 ||
-                                "Amount must be greater than 0",
-                            },
-                          }}
-                          render={({ field }) => <CurrencyInput {...field} />}
-                        />
-                        <div className="self-center max-sm:mt-auto mt-5">
-                          <button
-                            type="submit"
-                          >
-                            <img 
-                                src="/Stake/confirm.svg" 
-                                className="h-16 mt-6 cursor-pointer"
-                            />
-                          </button>
-                        </div>
-                    </form>
-                </div>
+export default function StakeScreen({ isOpen, onClose }: StakeScreenProps) {
+  const { control, handleSubmit } = useForm<DepositFormData>();
+  const [inputValue, setinputValue] = useState(0);
+  const { address } = useAccount();
+  const { data: nonce } = useReadContract({
+    abi: usdcAbi,
+    address: USDC,
+    functionName: "nonces",
+    args: [address!],
+  });
+
+  async function onSubmit(data: DepositFormData) {
+    const timestampInSeconds = Math.floor(Date.now() / 1000);
+    const deadline = BigInt(timestampInSeconds) + BigInt(EXPIRY);
+    const amount = serializeAmount(data.deposit.amount, USDC_DECIMAL);
+
+    console.log("Amount", amount);
+
+    const signature = await signTypedData(config, {
+      domain: {
+        name: "USDC",
+        chainId: baseSepolia.id,
+        verifyingContract: USDC,
+        version: "2",
+      },
+      types,
+      primaryType: "Permit",
+      message: {
+        owner: address!,
+        spender: EXECUTOR,
+        value: amount,
+        nonce: nonce!,
+        deadline,
+      },
+    });
+
+    const calls = await createMorphoCall(address!, amount, deadline, signature);
+    console.log("Calls", calls);
+    const tx = await execution(address!, calls);
+
+    await waitForTransactionReceipt(config, {
+      hash: tx,
+    });
+
+    console.log("Tx done");
+    console.log("Call tx", tx);
+  }
+
+  if (!isOpen) return null;
+
+  return (
+    <>
+      <div className="fixed inset-0  flex items-center justify-center z-50">
+        <div className="bg-gray-900/80 absolute t-0 left-0 w-full h-full z-0"></div>
+        <div className="bg-[#1E90FF] text-white rounded-lg shadow-lg p-6 w-[90vw] max-w-[600px] z-10 relative">
+          {/* Header */}
+          <div className="flex justify-center items-center border-b border-white pb-2">
+            <div className="items-center">
+              <span className="text-3xl inline-block">🛡️</span>
+              <img src="/morpho/eventInfo.svg" className="h-6 inline-block" />
             </div>
-            </div>
-        </>
-    );
-    }
+            <img
+              src="/morpho/cancel.svg"
+              className="h-10 absolute -right-2 top-2 cursor-pointer"
+              onClick={onClose}
+            />
+          </div>
+
+          {/* Body */}
+          <div className="mt-4 flex flex-col">
+            <img src="/Stake/information.svg" className="h-18" />
+            <img src="/Stake/intro.svg" className="h-10 mt-10" />
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="w-full flex flex-col gap-y-3"
+            >
+              <Controller
+                name="deposit"
+                control={control}
+                rules={{
+                  required: "Please enter an amount",
+                  validate: {
+                    positive: (value) =>
+                      parseFloat(value.amount) > 0 ||
+                      "Amount must be greater than 0",
+                  },
+                }}
+                render={({ field }) => <CurrencyInput {...field} />}
+              />
+              <div className="self-center max-sm:mt-auto mt-5">
+                <button type="submit">
+                  <img
+                    src="/Stake/confirm.svg"
+                    className="h-16 mt-6 cursor-pointer"
+                  />
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
